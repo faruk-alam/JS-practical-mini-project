ffd9e65217f05593b0cc5822337d3b4424913acd
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2022-01-19 22:01:07 +0100

    Add files via upload

---
+++ playing audio/app.js
@@ -0,0 +1,33 @@
+
+for(var i = 0; i < 4;i++){
+    document.querySelectorAll(".button")[i].addEventListener("click",function(){
+        var text = this.innerHTML;
+        console.log(text);
+        audioplay(text);
+    })
+}
+function audioplay(text) {
+    switch (text) {
+        case "A":
+            var audio = new Audio("mp3/Aa.mp3");
+            audio.play();
+            break;
+    
+        case "B":
+            var audio = new Audio("mp3/Bb.mp3");
+            audio.play();
+            break;
+    
+        case "C":
+            var audio = new Audio("mp3/Cc.mp3");
+            audio.play();
+            break;
+    
+        case "D":
+            var audio = new Audio("mp3/Dd.mp3");
+            audio.play();
+            break;
+    
+       
+    }
+}
